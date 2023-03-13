199505ee8d4af66b450a2db161df36788c3d0938
ml873129336 <873129336@qq.com>
2023-03-13 16:20:00 +0800

    修改

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -22,7 +22,7 @@ def topic(request,topic_id):
     topic = Topic.objects.get(id=topic_id)
     if topic.owner != request.user:
         raise  Http404
-    entries = topic.entry_set.order_by('-data_added')
+    entries = topic.entry_set.order_by('data_added')
     context = {'topic':topic,"entries":entries}
     return render(request,'learning_logs/topic.html',context)
 
